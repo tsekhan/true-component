12be9780156fdbb55b74717a0f929a18d278db48
Mikola Tsekhan <tsekhan@gmail.com>
2019-07-02 16:12:36 +0300

    JsDocs.

--- src/html/generateTagByKey.js
+++ src/html/generateTagByKey.js
@@ -1,3 +1,9 @@
+/**
+ * Generate HTML tag to use as placeholder instead of real data passed to `html` template literal tag.
+ *
+ * @param {string} key - key used for identifying of tag
+ * @returns {string} Returns HTML markup with generated tag.
+ */
 const generateTagByKey = key => `<template ${key}></template>`;
 
 export default generateTagByKey;
