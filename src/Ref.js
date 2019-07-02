f2ff52b3afb3a35c16a971efe49f8e97d94cfadc
Mikola Tsekhan <tsekhan@gmail.com>
2019-07-02 17:18:11 +0300

    JsDocs.

--- src/Ref.js
+++ src/Ref.js
@@ -1,1 +1,7 @@
+/**
+ * Purpose of the instance of `Ref` class is to provide access to HTML element created by `html` template tag.
+ *
+ * To make it work pass `Ref` instance to the `ref` attribute of your custom tag. After template tag was instantiated,
+ * attribute `node` of the `Ref` instance will point to the HTML element created by your custom tag.
+ */
 export default class Ref {}
--- src/nodeRegistry.js
+++ src/nodeRegistry.js
@@ -1,3 +1,8 @@
+/**
+ * Map tag name to class describing it.
+ *
+ * @type {Map<string, HtmlComponent>}
+ */
 const nodeRegistry = new Map();
 
 export default nodeRegistry;
