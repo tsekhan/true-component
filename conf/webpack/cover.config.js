69b0fb240acd95990dc7c9493e00daf6b1d113d6
Mikola Tsekhan <tsekhan@gmail.com>
2019-07-09 14:55:10 +0300

    Fix for wrong coverage: moved runner from mocha-puppeteer to mocha-headless-chrome.

---
+++ conf/webpack/cover.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const baseConfig = require('./base.config.js');
+
+module.exports = merge(baseConfig, {
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../../.dist_tmp'),
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'tests/test.html' },
+      { from: 'node_modules/mocha', to: 'test-resources/mocha' },
+      { from: 'node_modules/chai', to: 'test-resources/chai' },
+    ])
+  ],
+  entry: {
+    'tests': '.tmp/tests/index.js',
+  },
+});
