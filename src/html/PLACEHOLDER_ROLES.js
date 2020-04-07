7b82741d5ab20800f0bb86bc09281f901991e28b
Mickalaj Tsekhan <tsekhan@gmail.com>
2020-04-07 18:00:29 +0300

    Removed extra description from PLACEHOLDER_ROLES enum to optimize bundle size.

--- src/html/PLACEHOLDER_ROLES.js
+++ src/html/PLACEHOLDER_ROLES.js
@@ -10,40 +10,40 @@ const PLACEHOLDER_ROLES = {
   /**
    * Expression placed instead of tag name (like `<${expr}></${expr}>`).
    */
-  TAG_NAME: Symbol('TAG_NAME'),
+  TAG_NAME: Symbol(),
 
   /**
    * Disambiguation (role not detected precisely yet): expression placed instead of tag attribute (like
    * `<my-component ${expr}>...`) or tag attribute name (like `<my-component ${expr}="123">`).
    */
-  ATTRIBUTE_OR_ATTRIBUTE_NAME: Symbol('ATTRIBUTE_OR_ATTRIBUTE_NAME'),
+  ATTRIBUTE_OR_ATTRIBUTE_NAME: Symbol(),
 
   /**
    * Expression placed instead of whole attribute value: `<my-component attr="${expr}">`.
    */
-  ATTRIBUTE_VALUE: Symbol('ATTRIBUTE_VALUE'),
+  ATTRIBUTE_VALUE: Symbol(),
 
   /**
    * Expression placed instead of part of attribute name or attribute value: `<my-component attr-${expr}="123">...` or
    * `<my-component attr="123-${expr}">...`.
    */
-  PART_OF_ATTRIBUTE_OR_VALUE: Symbol('PART_OF_ATTRIBUTE_OR_VALUE'),
+  PART_OF_ATTRIBUTE_OR_VALUE: Symbol(),
 
 
   /**
    * Expression is placed instead of attribute name and attribute has no value: `<my-component ${expr}>...`.
    */
-  ATTRIBUTE: Symbol('ATTRIBUTE'),
+  ATTRIBUTE: Symbol(),
 
   /**
    * Expression is placed instead of tag attribute name (like `<my-component ${expr}="123">`).
    */
-  ATTRIBUTE_NAME: Symbol('ATTRIBUTE_NAME'),
+  ATTRIBUTE_NAME: Symbol(),
 
   /**
    * Expression is placed inside of other tag (or root element): `<my-component>${expr}</my-component>`
    */
-  TAG: Symbol('TAG'),
+  TAG: Symbol(),
 };
 
 export default PLACEHOLDER_ROLES;
